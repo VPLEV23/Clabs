c8d6abaae924f482e5dae619ec44cabddae5b277
Vlad <bladetheblade12@gmail.com>
2020-02-27 10:31:36 +0200

    Task1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace labC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            double p = (a + b + c) / 2;
+            double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            Console.WriteLine(s);
+        }
+    }
+}
