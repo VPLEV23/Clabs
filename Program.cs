3f4561caa85d4bdff0884545af5216d2e82b3163
Vlad <bladetheblade12@gmail.com>
2020-02-27 10:34:18 +0200

    Test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace labC
     {
         static void Main(string[] args)
         {
-
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            double p = (a + b + c) / 2;
+            double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            Console.WriteLine(s);
         }
     }
 }
